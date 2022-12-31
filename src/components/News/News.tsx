aea2c72ae87a669bac2fe0c0496bd0d19bd34f5f
ParLelya <parlelya@vk.com>
2022-12-31 14:18:37 +0300

    News

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -4,10 +4,10 @@ const News: React.FC = () => {
 	return (
 		<div id='news' className="row">
 			<h2 className="header">Актуальные новости</h2>
-			<div id="vk_post_-200001862_11385" className="col s12 m6" style={{ margin: '5px 10px' }}></div>
-			<div id="vk_post_-200001862_11340" className="col s12 m6" style={{ margin: '5px 10px' }}></div>
-			<div id="vk_post_-200001862_11322" className="col s12 m6" style={{ margin: '5px 10px' }}></div>
-			<div id="vk_post_-200001862_11284" className="col s12 m6" style={{ margin: '5px 10px' }}></div>
+			<div id="vk_post_-200001862_10474" className="col s12 m6" style={{ margin: '5px 10px' }}></div>
+			<div id="vk_post_-200001862_10403" className="col s12 m6" style={{ margin: '5px 10px' }}></div>
+			<div id="vk_post_-200001862_10394" className="col s12 m6" style={{ margin: '5px 10px' }}></div>
+			<div id="vk_post_-200001862_10387" className="col s12 m6" style={{ margin: '5px 10px' }}></div>
 		</div>
 	);
 };
