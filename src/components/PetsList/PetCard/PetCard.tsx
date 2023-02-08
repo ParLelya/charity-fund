7d9ea43aae032679f450e846220733e5f5dbf1de
ParLelya <parlelya@vk.com>
2023-02-08 17:18:45 +0300

    Working on pets list

--- src/components/PetsList/PetCard/PetCard.tsx
+++ src/components/PetsList/PetCard/PetCard.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
 
+interface IPets {
+	name: string,
+	age: string,
+	info: string,
+	id: number,
+	url: string
+}
+
 const PetCard: React.FC = () => {
+
+	const pets: IPets[] = [
+		{ name: 'Пуфик', age: '3 года', info: 'Кастрирован и привит.', id: 1, url: 'https://sun9-west.userapi.com/sun9-15/s/v1/ig2/zdwy5oFxFtyc1y0v0hbFpg_7s39XnqVL15ZLEaK3_nBbYKNaK5Kgpd62qtXeJ4yheUqXJUYeT5_DbckOE-E9Zgrs.jpg?size=2560x1706&quality=95&type=album' },
+		{ name: 'Бусинка', age: '2 года', info: 'Стерилизована и привита.', id: 2, url: 'https://sun9-west.userapi.com/sun9-54/s/v1/ig2/-uA8Nb-6XJ9_G39INzddKHsImSSMdczdbgCdAj8WdyHpGQBmNTHb3wdQZKcAfH9q8wZz6eYCG60cVjdRDaf1IBXg.jpg?size=2560x1706&quality=95&type=album' },
+		{ name: 'Якоб', age: '11 месяцев', info: 'Кастрирован и привит.', id: 3, url: 'https://sun9-east.userapi.com/sun9-22/s/v1/ig2/FdGkeIV2FTFEDX3sdt4hY5vkSzIRZ-zxa2JnKAQpGf4eLGAgN5FgMEfh71kSAse7T8AxnsPJrzj-CsvSFok0GAWf.jpg?size=2560x1706&quality=95&type=album' },
+		{ name: 'Рада', age: '11 месяцев', info: 'Стерилизована и привита.', id: 4, url: 'https://sun9-west.userapi.com/sun9-14/s/v1/ig2/x7BLkFqnJQN6qsPIamMA37xeJq2B0ZbY4PhPMggZhTLUL0RhwMcwA4OgnEp0P0LIR6QYJpupc27WI4HeskRt8-lp.jpg?size=2560x1706&quality=95&type=album' },
+	]
+
 	return (
-			<div className="col s12 m4 l3">
-				<div className="card pet">
-					<div className="card-image">
-						<img 
-						src="https://sun9-west.userapi.com/sun9-49/s/v1/ig2/I_yGl0rKMAXSMut3y8DIKJP1nbu2tjTNdaLveZ-AdYaUBIjAGdi2TYb8PqRQh09NJcF6Q6mYENYA4lNT0uqPl96o.jpg?size=2560x1707&quality=95&type=album" 
-						alt='cat'
-						/>
-						<span className="card-title">Кличка</span>
+		<div>
+			{pets.map(pet => {
+				return (
+					<div className="col s12 m6 l3">
+						<div key={pet.id} className="card pet">
+							<div className="card-image">
+								<img
+									src={pet.url}
+									alt='pet'
+								/>
+								<span className="card-title">{pet.name}, {pet.age}</span>
+							</div>
+							<div className="card-content">
+								<p>{pet.info}</p>
+							</div>
+							<div className="card-action">
+								<button className="waves-effect waves-light btn-small green lighten-2">Забрать домой</button>
+							</div>
+						</div>
 					</div>
-					<div className="card-content">
-						<p>Возраст</p>
-					</div>
-					<div className="card-action">
-						<a href="/">Подробнее</a>
-					</div>
-				</div>
-			</div>
+				)
+			})}
+		</div>
 	);
 };
 
--- src/components/PetsList/PetsList.tsx
+++ src/components/PetsList/PetsList.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import PetCard from './PetCard/PetCard';
 
+
+
 const PetsList: React.FC = () => {
+	
+
 	return (
 		<div id='pets' className="row">
 			<h2 className="header">Наши подопечные</h2>
-			<PetCard/>
-			<PetCard/>
-			<PetCard/>
-			<PetCard/>
-			<PetCard/>
 			<PetCard/>
 		</div>
 	);
