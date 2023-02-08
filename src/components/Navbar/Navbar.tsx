b5475b47e594d166564c4e472dcd18353a05e341
ParLelya <parlelya@vk.com>
2023-02-08 14:02:39 +0300

    Working with sidenav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,15 +3,20 @@ import React from 'react';
 const Navbar: React.FC = () => {
 	return (
 		<nav>
-			<div className="nav-wrapper green lighten-2" style={{position: 'sticky'}}>
-				<a href='/' className="brand-logo" style={{ left: '47%', top: '-20%'}}>
+			<div className="nav-wrapper green lighten-2">
+				<span className="left black-text hide-on-med-and-down" style={{ marginLeft: '10px', fontWeight: '600' }}>
+					Социальный центр спасения и реабилитации животных «Альтера Вита»
+				</span>
+				<a href='/' className="brand-logo" style={{ left: '47%', top: '-20%' }}>
 					<img
 						src='logo.png'
 						alt='logo'
-						style={{ width: '120px'}}
+						style={{ width: '120px' }}
 					/>
 				</a>
-
+				<a href="/" data-target="mobile-demo" className="sidenav-trigger">
+					<i className="material-icons black-text">menu</i>
+				</a>
 				<ul id="nav-mobile" className="right hide-on-med-and-down">
 					<li>
 						<a href="#about" className="black-text">
@@ -28,7 +33,7 @@ const Navbar: React.FC = () => {
 							<span>Новости</span>
 						</a>
 					</li>
-					<li style={{marginRight: '10px'}}>
+					<li style={{ marginRight: '10px' }}>
 						<a href="#help" className="black-text">
 							<span>Помочь</span>
 						</a>
